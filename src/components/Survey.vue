03c9f5ae0a4660cde11de3c43a566543ff48fbbc
NicKAnderson038 <nickanderson@gmail.com>
2018-01-20 12:12:04 -0800

    qrcode generator pop up model with url copy button

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -5,10 +5,28 @@
         <v-flex xs12 md6 offset-md3>
           <v-card>
             <v-toolbar color="indigo" dark>
-              <v-toolbar-title>CSHA Convention Survey</v-toolbar-title>
+              <!-- <v-toolbar-title>CSHA Convention Survey</v-toolbar-title> -->
+
+                   <!-- BEGIN: qrcode -->
+                   <v-toolbar-title>
+                <v-btn color="clear" large id="btn" flat dark @click.native.stop="dialog = true">CSHA Convention Survey</v-btn>
+      <v-dialog v-model="dialog" max-width="200">
+        <v-card>
+          <qriously :value="trueURL" :size="200"></qriously>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" flat="flat" v-clipboard="copyURL" @success="handleSuccess" @error="handleError" @click.native="dialog = false">Copy URL</v-btn>
+            <v-btn color="red darken-1" flat="flat" @click.native="dialog = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      </v-toolbar-title>
+<!-- END: qrcode -->
+
             </v-toolbar>
             <v-card-text>
               <span class="title"></span>
+
    <div id="app">
     <v-form v-model="valid" ref="form" lazy-validation>
       <v-select
@@ -79,6 +97,10 @@
 export default {
   data() {
     return {
+      copyURL: window.location.href,
+      dialog: false,
+      // trueURL: document.referrer,
+      trueURL: window.location.href,
       valid: true,
       name: "",
       nameRules: [
@@ -112,7 +134,19 @@ export default {
       console.error(error);
     }
   },
+  // computed: {
+  //   copy() {
+  //     console.log(document.referrer);
+  //     return this.trueURL;
+  //   }
+  // },
   methods: {
+    handleSuccess(e) {
+      console.log(`success: ${e.text}`);
+    },
+    handleError(e) {
+      console.log(`error: ${e}`);
+    },
     async submit() {
       const request = {
         method: "POST",
--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.css'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.css";
 
-import App from './App'
-import router from './router'
+import VueQriously from "vue-qriously";
+import VueClipboards from "vue-clipboards";
 
-Vue.use(Vuetify)
-Vue.config.productionTip = false
+import App from "./App";
+import router from "./router";
+
+Vue.use(Vuetify);
+Vue.use(VueQriously);
+Vue.use(VueClipboards);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
